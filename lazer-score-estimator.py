16d615f6ca343caaf3ee898301d989c3359ffb3d
Willy <11691902+willyosu@users.noreply.github.com>
2022-03-07 15:37:16 -0500

    Use object count instead of combo scale for score
    Keeping inline with [this change](https://github.com/ppy/osu/pull/16938) the calculator now uses object count of a map rather than the combo achieved for upwards scaling.

--- lazer-score-estimator.py
+++ lazer-score-estimator.py
@@ -6,9 +6,10 @@ class Score:
     comboPortion = 0.7
     scoreMultipliers = {"HD": 1.06, "HR": 1.06, "DT": 1.12, "FL": 1.12, "NF": 0.5, "EZ": 0.5, "HT": 0.3}
     
-    def __init__(self, maxCombo, accuracy = 100.0, combo = False, modString = False):
+    def __init__(self, maxCombo, hitObjects, accuracy = 100.0, combo = False, modString = False):
         self.scoreMultiplier = 1.0
         self.maxCombo = maxCombo
+        self.hitObjects = hitObjects
         self.accuracyRatio = accuracy / 100.0
 
         if(not combo):
@@ -31,29 +32,31 @@ class Score:
         self.standardizedScore = round((Score.maxScore * (Score.accuracyPortion * self.accuracyRatio + Score.comboPortion * self.comboRatio)) * self.scoreMultiplier)
 
     def calculateClassic(self):
-        self.classicScore = round(18 * pow((self.standardizedScore / Score.maxScore) * (self.maxCombo + 1), 2))
+        self.classicScore = round(36 * pow((self.standardizedScore / Score.maxScore) * (self.hitObjects + 1), 2))
 
 class CommandLine:
     def __init__(self):
         parser = argparse.ArgumentParser(description = "Description for my parser")
         parser.add_argument("-M", "--MaxCombo", help = "(int) maximum combo of a beatmap.", required = False)
+        parser.add_argument("-O", "--HitObjects", help = "(int) number of hit objects in a beatmap.", required = False)
         parser.add_argument("-a", "--accuracy", help = "(float) accuracy of a given play.", required = False, default = 100.0)
         parser.add_argument("-c", "--combo", help = "(int) combo achieved in a given play.", required = False, default = False)
         parser.add_argument("-m", "--mods", help = "(str) string of mods, using two char shorthand.", required = False, default = False)
         
         argument = parser.parse_args()
         if(argument.MaxCombo is not None):
-            play = Score(int(argument.MaxCombo), float(argument.accuracy), int(argument.combo), argument.mods)
+            play = Score(int(argument.MaxCombo), int(argument.HitObjects), float(argument.accuracy), int(argument.combo), argument.mods)
             print("Standardized: {0:,}\nClassic: {1:,}".format(play.standardizedScore, play.classicScore))
         else:
             self.runApplication()
 
     def runApplication(self):
         maxCombo = int(input("Max Combo: "))
+        hitObjects = int(input("Hit Objects: "))
         accuracy = float(input("Accuracy: "))
         combo = int(input("Combo: "))
         modList = input("Mods: ")
-        play = Score(maxCombo, accuracy, combo, modList)
+        play = Score(maxCombo, hitObjects, accuracy, combo, modList)
 
 if __name__ == '__main__':
     app = CommandLine()
